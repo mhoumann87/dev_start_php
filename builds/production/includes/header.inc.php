3445d0d6f445c7f74303f50584773e77921610fc
Michael Houmann <michael@michael-h.dk>
2017-08-07 13:02:49 +0200

    File structure created

---
+++ builds/production/includes/header.inc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="assets/css7style.css" href="/css/master.css">
+  </head>
+  <body>
+
+<header>
+
+</header>
