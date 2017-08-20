e9344e9db5da076567e799e23f2fd8c470dea93d
Michael Houmann <michael@michael-h.dk>
2017-08-20 11:07:25 +0200

    Browser sync works with php and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
+//pull in dependecies
 const   gulp = require('gulp'),
-        webserver = require('gulp-webserver'),
         postcss = require('gulp-postcss'),
         cssnext = require('postcss-cssnext'),
         gutil = require('gulp-util'),
         sourcemaps = require('gulp-sourcemaps'),
-        connect = require('gulp-connect-php'),
+		connect = require('gulp-connect-php'),
+		browserSync = require('browser-sync'),
 
         source = 'builds/development/',
-        dest = 'builds/production/assets/';
+		dest = 'builds/production/assets/';
+		
+		gulp.task('php', function() {
+			gulp.src('builds/production/*.php');
+		});
 
         gulp.task('css', function() {
 			gulp.src(source + 'css/style.css')
@@ -21,7 +26,26 @@ const   gulp = require('gulp'),
 				.pipe(gulp.dest(dest + 'css'));
 		});
 
+		gulp.task('connect-sync', function() {
+			connect.server({}, function (){
+			  browserSync({
+				proxy: 'http://localhost/1_starter_files/dev_start_php/builds/production'
+			  });
+			});
+
+		  });
+
+
         gulp.task('watch', function() {
-			gulp.watch(source + 'css/**/*.css', ['css']);
-			gulp.watch(dest + '**/*.php', ['php']);
-        });
+			gulp.watch(source + 'css/**/*.css', ['css']).on('change', function() {
+				browserSync.reload();
+			});
+			gulp.watch('builds/production/*.php', ['php']).on('change', function() {
+				browserSync.reload();
+			});
+		});
+
+		gulp.task('default', ['php', 'css', 'connect-sync', 'watch']);
+		
+		   
+	
